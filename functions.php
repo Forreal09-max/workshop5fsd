c5776908e654d0b638af39bea9b4b255ddbfd4c4
Rupesh kumar shrestha <shrestharupes2@gmail.com>
2025-12-24 07:37:20 +0545

    done ig

--- functions.php
+++ functions.php
@@ -1,40 +1,38 @@
 <?php
-
 function formatName($name) {
     return ucwords(trim($name));
 }
-
 function validateEmail($email) {
     return filter_var($email, FILTER_VALIDATE_EMAIL);
 }
-
 function cleanSkills($string) {
     $skills = explode(",", $string);
     return array_map("trim", $skills);
 }
-
 function saveStudent($name, $email, $skillsArray) {
     $data = $name . "|" . $email . "|" . implode(",", $skillsArray) . PHP_EOL;
     file_put_contents("students.txt", $data, FILE_APPEND);
 }
-
 function uploadPortfolioFile($file) {
-    $allowedTypes = ["application/pdf", "image/jpeg", "image/png"];
+
+    $allowedExtensions = ["pdf", "jpg", "jpeg", "png"];
     $maxSize = 2 * 1024 * 1024;
 
+    $ext = strtolower(pathinfo($file["name"], PATHINFO_EXTENSION));
+
+    if (!in_array($ext, $allowedExtensions)) {
+        throw new Exception("Invalid file type");
+    }
     if ($file["size"] > $maxSize) {
         throw new Exception("File size exceeds 2MB");
     }
-
-    if (!in_array($file["type"], $allowedTypes)) {
-        throw new Exception("Invalid file type");
+    if (!is_dir("uploads")) {
+        throw new Exception("Uploads folder not found");
     }
-
-    $ext = pathinfo($file["name"], PATHINFO_EXTENSION);
     $newName = "portfolio_" . time() . "." . $ext;
-
     if (!move_uploaded_file($file["tmp_name"], "uploads/" . $newName)) {
         throw new Exception("File upload failed");
     }
     return $newName;
 }
+
